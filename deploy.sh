a9adf5e921b4ad534a1cc4648f97c3e4fa4b5177
Xiao Zhang <zhang-x78@webmail.uwinnipeg.ca>
2024-08-10 00:45:16 -0500

    Remove untracked files

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Building app..."
-npm run build
-
-echo "Deploying files to server..."
-scp -i ~/.ssh/id_ed25519 -P <port> -r -v dist/* <user>@<ip>:/var/www/<domain.com>/
-
-echo "Done!"
